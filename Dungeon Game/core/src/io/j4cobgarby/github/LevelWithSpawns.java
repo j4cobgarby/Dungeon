a786898523fd4b1e2fbd653c73cf3a478b64c058
Jacob Garby <j.garby@icloud.com>
2017-01-09 10:10:46 +0000

    Update LevelWithSpawns.java

--- Dungeon Game/core/src/io/j4cobgarby/github/LevelWithSpawns.java
+++ Dungeon Game/core/src/io/j4cobgarby/github/LevelWithSpawns.java
@@ -2,6 +2,9 @@ package io.j4cobgarby.github;
 
 import com.badlogic.gdx.math.Vector2;
 
+// This class MUST be used instead of Level, when making a Level. This is because, with a Level, the
+// player wouldn't know where to appear!
+
 public class LevelWithSpawns extends Level {
 	Vector2 spawn;
 	
